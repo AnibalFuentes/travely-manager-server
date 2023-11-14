08812273dc153e890b4f3ad981e2bc64c77c7706
angelstchavez <angelstchavez@gmail.com>
2023-11-14 14:38:07 -0500

    feat: implement vehicle endpoints and services

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class BrandsService {
-  create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+  private readonly logger = new Logger('BrandsService');
+
+  constructor(
+    @InjectRepository(Brand)
+    private readonly brandRepository: Repository<Brand>,
+  ) {}
+
+  async create(createBrandDto: CreateBrandDto) {
+    const { name } = createBrandDto;
+
+    const existingBrand = await this.brandRepository.findOne({
+      where: { name },
+    });
+
+    if (existingBrand) {
+      throw new ConflictException('A brand with the same name already exists.');
+    }
+
+    try {
+      const brand = this.brandRepository.create(createBrandDto);
+      await this.brandRepository.save(brand);
+      return brand;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brandRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  async findOne(term: string) {
+    let brand: Brand;
+    if (isUUID(term)) {
+      brand = await this.brandRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder =
+        this.brandRepository.createQueryBuilder('documentType');
+      brand = await queryBuilder
+        .where('name = :term', {
+          term: term.toLowerCase(),
+        })
+        .getOne();
+    }
+    if (!brand) throw new NotFoundException(`Brand with ${term} not found`);
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  async update(id: string, updateBrandDto: UpdateBrandDto) {
+    const brand = await this.brandRepository.preload({
+      id: id,
+      ...updateBrandDto,
+    });
+
+    if (!brand) throw new NotFoundException(`Brand with id ${id} not found`);
+
+    try {
+      await this.brandRepository.save(brand);
+      return brand;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  async remove(id: string) {
+    const person = await this.findOne(id);
+    await this.brandRepository.remove(person);
+  }
+
+  private handleExceptions(error: any) {
+    this.logger.error(error);
+
+    throw new InternalServerErrorException(
+      'An unexpected error occurred. Please check server logs.',
+    );
   }
 }
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,26 +1,192 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { Repository } from 'typeorm';
+import { Vehicle } from './entities/vehicle.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BrandsService } from 'src/brands/brands.service';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class VehiclesService {
-  create(createVehicleDto: CreateVehicleDto) {
-    return 'This action adds a new vehicle';
+  private readonly logger = new Logger('VehiclesService');
+
+  constructor(
+    @InjectRepository(Vehicle)
+    private readonly vehicleRepository: Repository<Vehicle>,
+    private readonly brandsService: BrandsService,
+  ) {}
+
+  async create(createVehicleDto: CreateVehicleDto) {
+    const { brandId, plate, engineNumber, registrationCard } = createVehicleDto;
+
+    if (!isUUID(brandId, 4)) {
+      throw new BadRequestException(
+        'Invalid brandId. Debe ser un UUID válido.',
+      );
+    }
+
+    const brand = await this.brandsService.findOne(brandId);
+
+    if (!brand) {
+      throw new BadRequestException('La marca especificada no existe.');
+    }
+
+    const existingVehicleWithPlate = await this.vehicleRepository.findOne({
+      where: { plate },
+    });
+
+    if (existingVehicleWithPlate) {
+      throw new ConflictException(
+        'Ya existe un vehículo con la misma matrícula.',
+      );
+    }
+
+    const existingVehicleWithEngineNumber =
+      await this.vehicleRepository.findOne({
+        where: { engineNumber },
+      });
+
+    if (existingVehicleWithEngineNumber) {
+      throw new ConflictException(
+        'Ya existe un vehículo con el mismo número de motor.',
+      );
+    }
+
+    const existingVehicleWithRegistrationCard =
+      await this.vehicleRepository.findOne({
+        where: { registrationCard },
+      });
+
+    if (existingVehicleWithRegistrationCard) {
+      throw new ConflictException(
+        'Ya existe un vehículo con el mismo número de tarjeta de registro.',
+      );
+    }
+
+    try {
+      const vehicle = this.vehicleRepository.create(createVehicleDto);
+      await this.vehicleRepository.save(vehicle);
+      return vehicle;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all vehicles`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto || {};
+    return this.vehicleRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} vehicle`;
+  async findOne(term: string) {
+    let vehicle: Vehicle;
+
+    if (isUUID(term)) {
+      vehicle = await this.vehicleRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.vehicleRepository.createQueryBuilder('vehicle');
+      vehicle = await queryBuilder
+        .where(
+          'plate = :term OR registrationCard = :term OR engineNumber = :term',
+          {
+            term: term.toLowerCase(),
+          },
+        )
+        .getOne();
+    }
+
+    if (!vehicle)
+      throw new NotFoundException(`Vehículo con ${term} no encontrado`);
+    return vehicle;
   }
 
-  update(id: number, updateVehicleDto: UpdateVehicleDto) {
-    return `This action updates a #${id} vehicle`;
+  async findVehiclesByBrand(brandId: string) {
+    const brand = await this.brandsService.findOne(brandId);
+
+    if (!brand) {
+      throw new NotFoundException('Marca no encontrada.');
+    }
+
+    const vehicles = await this.vehicleRepository.find({
+      where: { brand: brand },
+    });
+    return vehicles;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} vehicle`;
+  async findVehiclesByModel(model: string) {
+    const vehicles = await this.vehicleRepository.find({ where: { model } });
+    return vehicles;
+  }
+
+  async findVehiclesByManufacturingYear(year: number) {
+    const vehicles = await this.vehicleRepository.find({
+      where: { manufacturingYear: year },
+    });
+    return vehicles;
+  }
+
+  async findVehiclesByColor(color: string) {
+    const vehicles = await this.vehicleRepository.find({ where: { color } });
+    return vehicles;
+  }
+
+  async findVehiclesByNumberOfSeats(seats: number) {
+    const vehicles = await this.vehicleRepository.find({
+      where: { numberOfSeats: seats },
+    });
+    return vehicles;
+  }
+
+  async findVehiclesByNumberOfAxles(axles: number) {
+    const vehicles = await this.vehicleRepository.find({
+      where: { numberAxles: axles },
+    });
+    return vehicles;
+  }
+
+  async findVehiclesByActivityStatus(isActive: boolean) {
+    const vehicles = await this.vehicleRepository.find({ where: { isActive } });
+    return vehicles;
+  }
+
+  async update(id: string, updateVehicleDto: UpdateVehicleDto) {
+    const vehicle = await this.vehicleRepository.preload({
+      id: id,
+      ...updateVehicleDto,
+    });
+
+    if (!vehicle)
+      throw new NotFoundException(`Vehículo con ID ${id} no encontrado`);
+
+    try {
+      await this.vehicleRepository.save(vehicle);
+      return vehicle;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  async remove(id: string) {
+    const vehicle = await this.findOne(id);
+    await this.vehicleRepository.remove(vehicle);
+  }
+
+  private handleExceptions(error: any) {
+    this.logger.error(error);
+
+    throw new InternalServerErrorException(
+      'Ocurrió un error inesperado. Por favor, revise los registros del servidor.',
+    );
   }
 }
