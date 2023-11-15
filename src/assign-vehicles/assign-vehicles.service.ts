258999a3aa58a9e824b4e23882c00132ac0d842a
angelstchavez <angelstchavez@gmail.com>
2023-11-15 03:11:27 -0500

    feat: Implement assing vehicles endpoints

--- src/assign-vehicles/assign-vehicles.service.ts
+++ src/assign-vehicles/assign-vehicles.service.ts
@@ -1,26 +1,153 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateAssignVehicleDto } from './dto/create-assign-vehicle.dto';
 import { UpdateAssignVehicleDto } from './dto/update-assign-vehicle.dto';
+import { AssignVehicle } from './entities/assign-vehicle.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { VehiclesService } from 'src/vehicles/vehicles.service';
+import { EmployeesDriversService } from 'src/employees-drivers/employees-drivers.service';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class AssignVehiclesService {
-  create(createAssignVehicleDto: CreateAssignVehicleDto) {
-    return 'This action adds a new assignVehicle';
+  private readonly logger = new Logger('AssignVehiclesService');
+
+  constructor(
+    @InjectRepository(AssignVehicle)
+    private readonly assignVehicleRepository: Repository<AssignVehicle>,
+    private readonly vehicleService: VehiclesService,
+    private readonly employeesDriversService: EmployeesDriversService,
+  ) {}
+
+  async create(createAssignVehicleDto: CreateAssignVehicleDto) {
+    const { vehicleId, driverOneId, driverTwoId } = createAssignVehicleDto;
+
+    if (!isUUID(vehicleId, 4)) {
+      throw new BadRequestException(
+        'Invalid vehicleId. Debe ser un UUID válido.',
+      );
+    }
+
+    const vehicle = await this.vehicleService.findOne(vehicleId);
+
+    if (!vehicle) {
+      throw new BadRequestException('El vehículo especificado no existe.');
+    }
+
+    if (!isUUID(driverOneId, 4)) {
+      throw new BadRequestException(
+        'Invalid driverOneId. Debe ser un UUID válido.',
+      );
+    }
+
+    const driverOne = await this.employeesDriversService.findOne(driverOneId);
+
+    if (!driverOne) {
+      throw new BadRequestException(
+        'El primer conductor especificado no existe.',
+      );
+    }
+
+    const driverTwo = driverTwoId
+      ? await this.employeesDriversService.findOne(driverTwoId)
+      : null;
+
+    if (driverTwoId && !driverTwo) {
+      throw new BadRequestException(
+        'El segundo conductor especificado no existe.',
+      );
+    }
+
+    try {
+      const assignVehicle = this.assignVehicleRepository.create({
+        vehicle,
+        driverOne,
+        driverTwo,
+        isActive: true,
+      });
+
+      await this.assignVehicleRepository.save(assignVehicle);
+      return assignVehicle;
+    } catch (error) {
+      this.logger.error(error);
+
+      if (error.code === '23505') {
+        throw new ConflictException(
+          'Ya existe una asignación de vehículo para este vehículo y conductores.',
+        );
+      }
+
+      throw new BadRequestException(
+        'Error al crear la asignación de vehículo. Por favor, revise los datos proporcionados.',
+      );
+    }
   }
 
   findAll() {
-    return `This action returns all assignVehicles`;
+    return this.assignVehicleRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} assignVehicle`;
+  findOne(id: string) {
+    return this.assignVehicleRepository.findOneBy({ id });
   }
 
-  update(id: number, updateAssignVehicleDto: UpdateAssignVehicleDto) {
-    return `This action updates a #${id} assignVehicle`;
+  async update(id: string, updateAssignVehicleDto: UpdateAssignVehicleDto) {
+    const assignVehicle = await this.assignVehicleRepository.findOneBy({ id });
+
+    if (!assignVehicle) {
+      throw new NotFoundException(
+        'La asignación de vehículo especificada no existe.',
+      );
+    }
+
+    const { driverOneId, driverTwoId } = updateAssignVehicleDto;
+
+    if (driverOneId) {
+      assignVehicle.driverOne =
+        await this.employeesDriversService.findOne(driverOneId);
+      if (!assignVehicle.driverOne) {
+        throw new BadRequestException(
+          'El primer conductor especificado no existe.',
+        );
+      }
+    }
+
+    if (driverTwoId) {
+      assignVehicle.driverTwo =
+        await this.employeesDriversService.findOne(driverTwoId);
+      if (driverTwoId && !assignVehicle.driverTwo) {
+        throw new BadRequestException(
+          'El segundo conductor especificado no existe.',
+        );
+      }
+    }
+
+    try {
+      await this.assignVehicleRepository.save(assignVehicle);
+      return assignVehicle;
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException(
+        'Error al actualizar la asignación de vehículo. Por favor, revise los datos proporcionados.',
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} assignVehicle`;
+  async remove(id: string) {
+    const assignVehicle = await this.assignVehicleRepository.findOneBy({ id });
+
+    if (!assignVehicle) {
+      throw new BadRequestException(
+        'La asignación de vehículo especificada no existe.',
+      );
+    }
+
+    await this.assignVehicleRepository.remove(assignVehicle);
   }
 }
--- src/assign-vehicles/dto/create-assign-vehicle.dto.ts
+++ src/assign-vehicles/dto/create-assign-vehicle.dto.ts
@@ -1,1 +1,27 @@
-export class CreateAssignVehicleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsUUID } from 'class-validator';
+
+export class CreateAssignVehicleDto {
+  @ApiProperty({
+    description: 'ID del vehículo asignado.',
+    example: 'e87ef3f1-1f2a-4b6f-b381-4ea3c40b6d3a',
+  })
+  @IsUUID()
+  vehicleId: string;
+
+  @ApiProperty({
+    description: 'ID del conductor principal asignado al vehículo.',
+    example: 'e87ef3f1-1f2a-4b6f-b381-4ea3c40b6d3a',
+  })
+  @IsUUID()
+  driverOneId: string;
+
+  @ApiProperty({
+    description: 'ID del conductor secundario asignado al vehículo.',
+    example: 'e87ef3f1-1f2a-4b6f-b381-4ea3c40b6d3a',
+    required: false,
+  })
+  @IsUUID()
+  @IsOptional()
+  driverTwoId?: string;
+}
