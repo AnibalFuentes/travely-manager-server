877b1583a0f2f48cf04efad625d558eb68b4c66e
angelstchavez <angelstchavez@gmail.com>
2023-11-21 01:23:56 -0500

    fix: update vehicle dto

--- src/vehicles/dto/create-vehicle.dto.ts
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -1,24 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsInt,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  IsUUID,
-  Max,
-  Min,
-} from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateVehicleDto {
-  @ApiProperty({
-    description: 'Número de la tarjeta de registro del vehículo.',
-    example: 'AB123CD',
-  })
-  @IsOptional()
-  @IsString()
-  @IsNotEmpty()
-  registrationCard?: string;
-
   @ApiProperty({
     description: 'Número de matrícula del vehículo.',
     example: 'XYZ789',
@@ -28,32 +11,22 @@ export class CreateVehicleDto {
   plate: string;
 
   @ApiProperty({
+    description: 'Referencia del vehículo.',
+    example: 'Corolla',
+  })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  reference?: string;
+
+  @ApiProperty({
     description: 'Nombre del modelo del vehículo.',
-    example: 'Transit 2022',
+    example: '2022',
   })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   model?: string;
-
-  @ApiProperty({
-    description: 'Número de motor del vehículo.',
-    example: 'E123456789CAR4D',
-  })
-  @IsOptional()
-  @IsString()
-  @IsNotEmpty()
-  engineNumber?: string;
-
-  @ApiProperty({
-    description: 'Año de fabricación del vehículo.',
-    example: 2020,
-  })
-  @IsOptional()
-  @IsInt()
-  @Min(1900)
-  @Max(new Date().getFullYear())
-  manufacturingYear?: number;
 
   @ApiProperty({ example: 'Autobus', description: 'tipo de vehículo' })
   @IsOptional()
